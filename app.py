bde220e6805a6884b4db057abc2e2319b035cbe2
aithasaipriya <87469361+aithasaipriya@users.noreply.github.com>
2021-07-17 20:33:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template , request
+app = Flask(__name__)
+import pickle
+model = pickle.load(open('dtr.pkl','rb'))
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+@app.route('/guest',methods=["POST"])
+def Guest():
+        year = request.form["year"]
+        month = request.form["month"]
+        day = request.form["day"]
+        data = [[year,month,day]]
+        prediction = model.predict(data)
+        prediction = prediction[0]
+        return render_template("index.html",y= "the gas price is"+str(prediction))
+@app.route('/use')
+def user():
+    return"hello user" 
+
+if __name__ == '__main__':
+     app.run(debug = True)   
+   
+    
